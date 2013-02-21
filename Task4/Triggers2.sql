148b5f49116f3345e7a94d1117b85536c874ef54
Tomas <tomsel1@hotmail.com>
2013-02-21 09:10:58 +0100

    test data

--- Task4/Triggers2.sql
+++ Task4/Triggers2.sql
@@ -75,8 +75,20 @@ BEGIN
 	END IF;
 END;
 
+INSERT INTO Registrations(Student, courses) VALUES (123, 'ELA222'); -- Ska funka eftersom 123 läst ELA111 detta testar RequiredCourse
+INSERT INTO Registrations(Student, courses) VALUES (123, 'ELA111'); -- ska ej läggas in eftersom 123 redan läst kursen
+INSERT INTO Registrations(Student, courses) VALUES (111, 'MVE111'); -- ska ej funka redan registrerad
+INSERT INTO Registrations(Student, courses) VALUES (999, 'TDA416'); -- ska bli registrerad
+INSERT INTO Registrations(Student, courses) VALUES (666, 'TDA416'); -- hamna i väntelista
+INSERT INTO Registrations(Student, courses) VALUES (777, 'TDA416'); -- hamna i väntelitsa
+INSERT INTO Registrations(Student, courses) VALUES (777, 'MVE111'); -- bli registrerad
+
+
+
 INSERT INTO Registrations(Student, courses) VALUES (123, 'TDA999');
 INSERT INTO Registrations(Student, courses) VALUES (123, 'ELA111');
+
+
 
 /*
 
