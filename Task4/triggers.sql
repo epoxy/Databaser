84015f9160ca3395788635c892a01ac374580d43
epoxy <palmqvist.anton@gmail.com>
2013-02-21 08:22:54 +0100

    commit

--- Task4/triggers.sql
+++ Task4/triggers.sql
@@ -1,1 +1,10 @@
- 
+  CREATE TRIGGER TryToRegister
+  	AFTER INSERT ON 
+
+
+CREATE ASSERTION AlreadyPassed
+CHECK(NOT EXISTS
+	(SELECT Registred.student student1, WaitingFor.student student2
+	FROM Registred, WaitingFor
+	WHERE student1=student1)
+);
