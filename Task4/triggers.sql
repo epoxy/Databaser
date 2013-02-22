071c8b7ece7e78eba9fd0cbc1120f087fbb70431
epoxy <palmqvist.anton@gmail.com>
2013-02-22 15:16:22 +0100

    exception added

--- Task4/triggers.sql
+++ Task4/triggers.sql
@@ -1,3 +1,86 @@
+CREATE OR REPLACE TRIGGER RegAtStudent
+INSTEAD OF INSERT ON Registrations
+REFERENCING NEW AS new
+FOR EACH ROW
+DECLARE 
+	requiredCourse INT;
+	passedRequiredCourse INT;
+	passedCurrentCourse INT;
+	alreadyRegistred INT;
+	IsaLimitedCourse INT;
+	nbrOfRegistredInCourse INT;
+	NbrOfPlacesInCourse INT;
+	
+	
+BEGIN
+	SELECT COUNT(*)
+	INTO requiredCourse
+	FROM Requires R
+	WHERE R.course = :new.Courses;
+
+	SELECT COUNT(*)
+	INTO passedRequiredCourse
+	FROM Requires R, PassedCourses P
+	Where R.requiredCourse = P.passedCourse
+	AND P.StudentID = :new.student
+	AND R.Course = :new.Courses;
+	
+	SELECT COUNT (*)
+	INTO passedCurrentCourse 
+	FROM PassedCourses P
+	WHERE P.passedCourse = :new.Courses
+	AND P.StudentID = :new.student;
+
+	IF passedCurrentCourse=0 
+	THEN
+      raise_application_error (-20999,'Student has already passed the course');
+	END IF;
+	
+	SELECT COUNT (*)
+	INTO alreadyRegistred
+	FROM Registrations R
+	WHERE R.Courses = :new.Courses
+	AND R.Status = 'registered'
+	AND R.Student = :new.student;
+	
+	SELECT Count(*)
+	INTO IsaLimitedCourse
+	FROM LimitedParticipantsCourse L
+	WHERE L.course = :new.Courses;
+	
+	SELECT COUNT (*)
+	INTO nbrOfRegistredInCourse
+	FROM Registrations R
+	WHERE R.Courses = :new.Courses
+	AND R.Status = 'registered';
+	
+
+	
+	IF requiredCourse = passedRequiredCourse THEN
+		IF passedCurrentCourse = 0 THEN
+			IF alreadyRegistred = 0 THEN
+				IF IsaLimitedCourse != 0 THEN
+					SELECT L.availablePlaces
+					INTO NbrOfPlacesInCourse
+					FROM LimitedParticipantsCourse L
+					WHERE L.course = :new.Courses;	
+					IF nbrOfRegistredInCourse >= NbrOfPlacesInCourse THEN
+						INSERT INTO WaitingFor
+						VALUES (:new.student, :new.Courses, current_TIMESTAMP);	
+					ELSE
+						INSERT INTO Registred
+						VALUES (:new.Student, :new.Courses);
+				 	END IF;
+				ELSE
+				INSERT INTO Registred
+				VALUES (:new.Student, :new.Courses);
+				END IF;
+			END IF;
+		END IF;
+	END IF;
+END;
+
+
 CREATE OR REPLACE TRIGGER UnRegStudent
 INSTEAD OF DELETE ON Registrations
 REFERENCING OLD AS old
@@ -141,26 +224,25 @@ SELECT COUNT (*)
 		DELETE
 		FROM Registrations R
 		WHERE R.Courses = :old.Courses
-		AND R.student = :old.student;
-	END IF;
-	
-	IF nbrOfStudentWaiting != 0 AND IsaLimitedCourse != 0 THEN 
-		SELECT L.availablePlaces
-		INTO NbrOfPlacesInCourse
-		FROM LimitedParticipantsCourse L
-		WHERE L.course = :old.Courses;
-		IF nbrOfRegistredInCourse < NbrOfPlacesInCourse THEN
-			SELECT  q.student
-			INTO registerStudent
-			FROM CourseQueuePositions q
-			WHERE q.Course = :old.courses
-			AND q.queueNumber = 1;
-			INSERT INTO Registred
-			VALUES (registerStudent, :old.courses);
-			DELETE 
-			FROM CourseQueuePositions q
-			WHERE q.Course = :old.courses
-			AND q.queueNumber = 1;					
+		AND R.student = :old.student;	
+		IF nbrOfStudentWaiting != 0 AND IsaLimitedCourse != 0 THEN 
+			SELECT L.availablePlaces
+			INTO NbrOfPlacesInCourse
+			FROM LimitedParticipantsCourse L
+			WHERE L.course = :old.Courses;
+			IF nbrOfRegistredInCourse < NbrOfPlacesInCourse THEN
+				SELECT  q.student
+				INTO registerStudent
+				FROM CourseQueuePositions q
+				WHERE q.Course = :old.courses
+				AND q.queueNumber = 1;
+				INSERT INTO Registred
+				VALUES (registerStudent, :old.courses);
+				DELETE 
+				FROM CourseQueuePositions q
+				WHERE q.Course = :old.courses
+				AND q.queueNumber = 1;
+			END IF;					
 		END IF;
 	END IF;	
 END;
