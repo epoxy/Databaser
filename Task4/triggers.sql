a907336993a4bd98bf1607d419a6162607a642d9
Tomas <tomsel1@hotmail.com>
2013-02-22 14:50:29 +0100

    ..

--- Task4/triggers.sql
+++ Task4/triggers.sql
@@ -20,6 +20,7 @@ DECLARE
 	nbrOfRegistredInCourse INT;
 	nbrOfPlacesInCourse INT;
 	registerStudent INT;
+	nbrOfStudentsInQueue INT;
 
 BEGIN
 SELECT COUNT (*)
@@ -46,6 +47,11 @@ SELECT COUNT (*)
 	FROM Registrations R
 	WHERE R.Courses = :old.Courses
 	AND R.Status = 'registered';
+	
+	SELECT COUNT(*)
+	INTO nbrOfStudentsInQueue
+	FROM CourseQueuePositions Q
+	WHERE Q.Course = old.Courses;
 	
 	IF alreadyWaiting != 0 THEN	
 	DELETE
@@ -76,4 +82,85 @@ SELECT COUNT (*)
 			VALUES (registerStudent, :old.courses);
 		END IF;
 	END IF;
+END;
+
+---- latest
+
+CREATE OR REPLACE TRIGGER UnRegStudent
+INSTEAD OF DELETE ON Registrations
+REFERENCING OLD AS old
+FOR EACH ROW
+DECLARE
+	alreadyRegistred INT;
+	alreadyWaiting INT;
+	IsaLimitedCourse INT;
+	nbrOfRegistredInCourse INT;
+	nbrOfPlacesInCourse INT;
+	registerStudent INT;
+	nbrOfStudentWaiting INT;
+
+BEGIN
+SELECT COUNT (*)
+	INTO alreadyRegistred
+	FROM Registrations R
+	WHERE R.Courses = :old.Courses
+	AND R.Status = 'registered'
+	AND R.Student = :old.student;
+
+SELECT COUNT (*)
+	INTO alreadyWaiting
+	FROM Registrations R
+	WHERE R.Courses = :old.Courses
+	AND R.Status = 'waiting'
+	AND R.Student = :old.student;
+	
+	SELECT Count(*)
+	INTO IsaLimitedCourse
+	FROM LimitedParticipantsCourse L
+	WHERE L.course = :old.Courses;
+	
+	SELECT COUNT (*)
+	INTO nbrOfRegistredInCourse
+	FROM Registrations R
+	WHERE R.Courses = :old.Courses
+	AND R.Status = 'registered';
+	
+	SELECT COUNT (*)
+	INTO nbrOfStudentWaiting
+	FROM CourseQueuePositions Q
+	WHERE q.Course = :old.courses;
+	
+	IF alreadyWaiting != 0 THEN	--sista satsen vi gör!
+		DELETE
+		FROM Registrations R
+		WHERE R.Courses = :old.courses
+		AND R.student = :old.student;
+	END IF;
+	
+	IF alreadyRegistred != 0 THEN -- kasta exception?
+		DELETE
+		FROM Registrations R
+		WHERE R.Courses = :old.Courses
+		AND R.student = :old.student;
+	END IF;
+	
+	IF nbrOfStudentWaiting != 0 AND IsaLimitedCourse != 0 THEN 
+		SELECT L.availablePlaces
+		INTO NbrOfPlacesInCourse
+		FROM LimitedParticipantsCourse L
+		WHERE L.course = :old.Courses;
+		IF nbrOfRegistredInCourse < NbrOfPlacesInCourse THEN
+			SELECT  q.student
+			INTO registerStudent
+			FROM CourseQueuePositions q
+			WHERE q.Course = :old.courses
+			AND q.queueNumber = 1;
+			INSERT INTO Registred
+			VALUES (registerStudent, :old.courses);
+			DELETE 
+			FROM CourseQueuePositions q
+			WHERE q.Course = :old.courses
+			AND q.queueNumber = 1;					
+		END IF;
+	END IF;	
 END;
