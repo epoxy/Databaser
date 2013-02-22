2ad0a2a4c9016bb1e4d2e6ea9f7b1d74279e8f06
epoxy <palmqvist.anton@gmail.com>
2013-02-22 16:17:46 +0100

    commit

--- Task4/triggertest.sql
+++ Task4/triggertest.sql
@@ -5,8 +5,9 @@ INSERT INTO Registrations(Student, courses) VALUES (999, 'TDA416'); -- ska bli r
 INSERT INTO Registrations(Student, courses) VALUES (666, 'TDA416'); -- hamna i väntelista
 INSERT INTO Registrations(Student, courses) VALUES (777, 'TDA416'); -- hamna i väntelitsa
 INSERT INTO Registrations(Student, courses) VALUES (777, 'MVE111'); -- bli registrerad
-INSERT INTO Registrations(Student, courses) VALUES (777, 'MVE111'); -- Exception: "Student is already registered to the course"
+INSERT INTO Registrations(Student, courses) VALUES (777, 'MVE111'); -- Exception: "Student is already registered to or waiting for the course"
 INSERT INTO Registrations(Student, courses) VALUES (111, 'DAT111'); -- Exception: "Student has already passed the course"
+INSERT INTO Registrations(Student, courses) VALUES (111, 'DAT111'); -- Exception: "Student hasn´t passed the required courses"
 
 
 DELETE --222 should be unregisterd from course and 666 should be registred
